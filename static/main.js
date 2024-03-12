bec4ae7a788e3048120355d9fc34b10b2a38d9d6
samihalawa <samihalawaster@gmail.com>
2024-03-12 22:05:17 +0100

    m

--- static/main.js
+++ static/main.js
@@ -23,15 +23,18 @@ $(document).ready(function() {
             $('#myModal').css('display', 'none');
         }
     });
-
-    function loadEmails() {
-        $.get('/get_emails', function(data) {
-            var emails = data.split('\n');
-            var modalBody = $('#modalBody');
-            modalBody.empty();
-            for (var i = emails.length - 1; i >= 0; i--) {
-                var emailInfo = emails[i].split(',');
-                var email = emailInfo[1].trim();
+function loadEmails() {
+    $.get('/get_emails', function(data) {
+        var emails = data.split('\n');
+        var modalBody = $('#modalBody');
+        modalBody.empty();
+        var uniqueDomains = {}; // Objeto para almacenar los dominios únicos y sus correos electrónicos correspondientes
+        for (var i = 0; i < emails.length; i++) {
+            var emailInfo = emails[i].split(',');
+            var email = emailInfo[1].trim();
+            var domain = email.split('@')[1]; // Obtener el dominio del correo electrónico
+            // Verificar si el correo electrónico tiene un formato válido y no está en la lista de dominios repetidos
+            if (isValidEmail(email) && !(domain in uniqueDomains)) {
                 var page_title = emailInfo[2].trim();
                 var url = emailInfo[3].trim();
                 var resultElement = $('<div>').addClass('p-4 bg-white shadow-md mb-4');
@@ -39,10 +42,21 @@ $(document).ready(function() {
                 resultElement.append($('<p>').addClass('text-sm italic').text('页面标题: ' + page_title));
                 resultElement.append($('<p>').addClass('text-xs').text('网址: ' + url));
                 modalBody.append(resultElement);
+                // Agregar el dominio al objeto de dominios únicos
+                uniqueDomains[domain] = true;
             }
-        });
-    }
+        }
+    });
+}
 
+// Función auxiliar para verificar si un string es un email válido
+function isValidEmail(email) {
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+
+    
     socket.on('new_email', function(data) {
         var resultElement = $('<div>').addClass('p-4 bg-white shadow-md mb-4');
         resultElement.append($('<p>').addClass('text-lg font-bold').text('邮箱: ' + data.email));
