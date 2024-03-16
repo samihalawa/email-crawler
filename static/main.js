2dd2ab47a1a6a1110bfbf28adc16436e3934a4a2
System Administrator <root@mac.local>
2024-03-16 20:38:40 +0100

    m

--- static/main.js
+++ static/main.js
@@ -47,7 +47,7 @@ $(document).ready(function() {
                     resultElement.append($('<p>').addClass('text-sm italic').text(page_title));
                     resultElement.append($('<p>').addClass('text-xs').text(url));
                     var copyButton = $('<button>').addClass('absolute top-0 right-0 mt-1 mr-1 p-1 bg-gray-200 rounded text-gray-600 hover:bg-gray-300 hover:text-gray-800 focus:outline-none');
-                    copyButton.html('<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2m-4 0V3H5a2 2 0 00-2 2v12a2 2 0 002 2h2v4l7-5-7-5v4z" /></svg>');
+                    copyButton.html('<span>Send Mail</span><svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2m-4 0V3H5a2 2 0 00-2 2v12a2 2 0 002 2h2v4l7-5-7-5v4z" /></svg>');
                     copyButton.click(function() {
                         var emailText = $(this).siblings('p.text-lg.font-bold').text().replace('Email: ', '');
                         navigator.clipboard.writeText(emailText).then(function() {
@@ -81,7 +81,7 @@ $(document).ready(function() {
             resultElement.append($('<p>').addClass('text-sm italic').text(data.page_title));
             resultElement.append($('<p>').addClass('text-xs').text(data.url));
             var copyButton = $('<button>').addClass('absolute top-0 right-0 mt-1 mr-1 p-1 bg-gray-200 rounded text-gray-600 hover:bg-gray-300 hover:text-gray-800 focus:outline-none');
-            copyButton.html('<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2m-4 0V3H5a2 2 0 00-2 2v12a2 2 0 002 2h2v4l7-5-7-5v4z" /></svg>');
+            copyButton.html('<span>Send Mail</span> <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2m-4 0V3H5a2 2 0 00-2 2v12a2 2 0 002 2h2v4l7-5-7-5v4z" /></svg>');
             copyButton.click(function() {
                 var emailText = $(this).siblings('p.text-lg.font-bold').text().replace('Email: ', '');
                 navigator.clipboard.writeText(emailText).then(function() {
